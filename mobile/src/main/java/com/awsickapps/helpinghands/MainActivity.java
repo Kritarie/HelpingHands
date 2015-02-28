a66f98c6a9181d5e8ed1616d841170ab019b2001
Allen Calderwood <calderwoodra@vcu.edu>
2015-02-28 13:34:49 -0500

    updated nav drawer sections

--- mobile/src/main/java/com/awsickapps/helpinghands/MainActivity.java
+++ mobile/src/main/java/com/awsickapps/helpinghands/MainActivity.java
@@ -57,17 +57,8 @@ public class MainActivity extends ActionBarActivity
     }
 
     public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
+        String[] sections = getResources().getStringArray(R.array.nav_drawer_sections);
+        mTitle = sections[number - 1];
     }
 
     public void restoreActionBar() {
